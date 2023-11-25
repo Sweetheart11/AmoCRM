9049150370ccc929a71ddc1c8f7f628ac24cdf09
Kanostepp <74007817+Sweetheart11@users.noreply.github.com>
2023-11-25 20:01:55 +0300

    Update AmoCRMCurlService.php

--- app/Services/AmoCRMCurlService.php
+++ app/Services/AmoCRMCurlService.php
@@ -9,17 +9,6 @@ use App\Exceptions\AmoCRMAuthException;
 
 class AmoCRMCurlService implements CRMCurlServiceInterface
 {
-    private $errors = [
-        301 => 'Moved permanently.',
-        400 => 'Wrong structure of the array of transmitted data, or invalid identifiers of custom fields.',
-        401 => 'Not Authorized. There is no account information on the server. You need to make a request to another server on the transmitted IP.',
-        403 => 'The account is blocked, for repeatedly exceeding the number of requests per second.',
-        404 => 'Not found.',
-        500 => 'Internal server error.',
-        502 => 'Bad gateway.',
-        503 => 'Service unavailable.'
-    ];
-
     public function __construct(
         public string $subdomain,
         public int $pipeline_id,
